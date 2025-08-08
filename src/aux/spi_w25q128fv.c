62e1a1b4ce98063b40e83d4f30d95a159e6a8f21
Alessandro Nardinelli <anardine.ef@gmail.com>
2025-08-07 21:20:33 -0300

    adjusted type conversion when reading and writing to SPI

--- src/aux/spi_w25q128fv.c
+++ src/aux/spi_w25q128fv.c
@@ -4,15 +4,16 @@
 void wait_for_clearance(SPI_Handler_t *pToSPIx) {
         uint8_t status;
     do {
-        spi_write(pToSPIx, 0x05, 1); // Read Status Register-1 command
+        uint8_t address = 0x05;
+        spi_write(pToSPIx,&address, 1); // Read Status Register-1 command
         status = spi_read(pToSPIx);
     } while (status & 0x01); // WIP bit is set if busy
 }
 
 uint8_t check_write_clearance(SPI_Handler_t *pToSPIx) {
     uint8_t status;
-
-        spi_write(pToSPIx, 0x05, 1); // Read Status Register-1 command
+    uint8_t address = 0x05;
+        spi_write(pToSPIx, &address, 1); // Read Status Register-1 command
         status = spi_read(pToSPIx);
     
         if(status & 2) {
@@ -30,8 +31,8 @@ uint8_t flash_read_data(SPI_Handler_t *pToSPIx, uint8_t *blockAddress, uint32_t 
     GPIOB->port &= ~(1 << 2);
 
     wait_for_clearance(pToSPIx);
-
-    spi_write(pToSPIx, 0x03, 1); // sends the read command
+    uint8_t address = 0x03;
+    spi_write(pToSPIx, &address, 1); // sends the read command
     spi_write(pToSPIx, blockAddress, 3); // send the block address where the data is stored
 
     for(uint32_t i=0; i<length; i++) {
@@ -59,13 +60,13 @@ void flash_write_data(SPI_Handler_t *pToSPIx, uint8_t *blockAddress, uint32_t le
     wait_for_clearance(pToSPIx); // device ready
 
     if(check_write_clearance(pToSPIx)) { // device write enabled if 0
-
-        spi_write(pToSPIx, 0x06, 1); // Write Enable instruction
+        uint8_t address = 0x06;
+        spi_write(pToSPIx, &address, 1); // Write Enable instruction
 
     } 
-        
+        uint8_t address = 0x02;
         // Page Program command (0x02)
-        spi_write(pToSPIx, 0x02, 1);
+        spi_write(pToSPIx, &address, 1);
         // Send 3-byte address
         spi_write(pToSPIx, blockAddress, 3);
         // Write data bytes
@@ -87,8 +88,8 @@ void flash_erase(SPI_Handler_t *pToSPIx, uint8_t *blockAddress, uint32_t length,
     wait_for_clearance(pToSPIx); // device ready
 
     if(check_write_clearance(pToSPIx)) { // device write enabled
-
-        spi_write(pToSPIx, 0x06, 1); // Write Enable instruction
+        uint8_t address = 0x06;
+        spi_write(pToSPIx, &address, 1); // Write Enable instruction
 
     }
 
@@ -96,9 +97,9 @@ void flash_erase(SPI_Handler_t *pToSPIx, uint8_t *blockAddress, uint32_t length,
     // Here, we'll use Sector Erase (0x20) for the address provided
     // You can change to 0xD8 for 64KB block erase or 0xC7 for chip erase
 
-    // Sector Erase command (0x20)
-    spi_write(pToSPIx, eraseMode, 1); // Send erase command
-    spi_write(pToSPIx, blockAddress, 3); // Send 3-byte address
+    // Sector Erase command (0x20) REVIEW CODE
+    //spi_write(pToSPIx, eraseMode, 1); // Send erase command
+    //spi_write(pToSPIx, blockAddress, 3); // Send 3-byte address
 
     GPIOB->port |= (1 << 2); // set CS to high. Setting CS high is a must for completing the erasing cycle.
 }
