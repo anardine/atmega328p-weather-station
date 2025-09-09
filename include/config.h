423d9693ac1f42a29adbff20ca5c210d02c873b8
Alessandro Nardinelli <anardine.ef@gmail.com>
2025-09-09 15:45:18 -0300

    example of config.h added

---
+++ include/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID           "LALALALALA"
+#define WIFI_PWD            "LALALLALALA"
+
+#define WEB_HOST            "LALALLA.LALALA.COM/LALALA"
+#define SCHEMA              "LALALALLA"
+#define DB                  "lalalala_lalalal"
+
+#define DB_MASTER           "lalalala_lalala"
+#define DB_PWD              "lalalallalala"
+
+#endif // CONFIG_H
